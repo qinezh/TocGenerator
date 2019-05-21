0a905ae5e7db2d1441a8d5c63eed70400d0c43ec
Qinen Zhu <qinzhu@microsoft.com>
2019-05-21 10:01:40 +0800

    Add README.md

--- Program.cs
+++ Program.cs
@@ -1,23 +1,52 @@
-﻿using System;
+﻿using Microsoft.DocAsCode.Common;
+using System;
 using System.IO;
 
 namespace TocGenerator
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            var rootFolder = "E:/temp/blog";
-            var rootName = "TocRoot";
-            var tocPath = Path.Combine("E:/temp", "toc.yml");
-            var tocViewModel = TocHelper.GenerateTocFromDirectory(rootFolder, rootName);
+            if (args.Length < 1)
+            {
+                PrintUsage();
+                return 2;
+            }
 
-            TocHelper.ResolveHref(tocPath, tocViewModel);
-            TocHelper.SortTocItems(tocViewModel);
-            TocHelper.DistinctTocItems(tocViewModel);
-            TocHelper.SaveToc(tocPath, tocViewModel);
+            try
+            {
+                var rootFolder = new DirectoryInfo(args[0]);
+                var rootName = rootFolder.Name;
+                var rootPath = PathUtility.MakeRelativePath(AppDomain.CurrentDomain.BaseDirectory, rootFolder.FullName);
+                var tocPath = args.Length > 1 ? args[1] : Path.Combine(rootPath, "toc.yml");
+                var tocViewModel = TocHelper.GenerateTocFromDirectory(rootPath, rootName);
 
-            Console.WriteLine("Finished");
+                if (tocViewModel == null)
+                {
+                    Console.WriteLine($"no content under {rootFolder.FullName}");
+                    return 2;
+                }
+
+                TocHelper.ResolveHref(tocPath, tocViewModel);
+                TocHelper.SortTocItems(tocViewModel);
+                TocHelper.DistinctTocItems(tocViewModel);
+                TocHelper.SaveToc(tocPath, tocViewModel);
+
+                Console.WriteLine($"toc.yml is generated at {tocPath}");
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+                return 1;
+            }
+        }
+
+        static void PrintUsage()
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine($"\t{AppDomain.CurrentDomain.FriendlyName} <path_of_folder> [path_of_toc]");
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,10 @@
+﻿# TocGenerator
+
+Generate toc.yml used by docfx based on folder structure 
+
+## Usage
+
+```cmd
+dotnet restore
+dotnet run <path_of_folder> [path_of_toc]
+```
